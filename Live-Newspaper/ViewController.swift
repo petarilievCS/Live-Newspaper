2f08f23c1caecab15cf3c63ad9868e913cfdf997
Petar Iliev <petariliev2002@gmail.com>
2022-11-22 09:05:13 -0600

    Implement video

--- Live-Newspaper/ViewController.swift
+++ Live-Newspaper/ViewController.swift
@@ -54,12 +54,21 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Create plane for image
         if let imageAnchor = anchor as? ARImageAnchor {
             
+            // Create video and position it
+            let videoNode = SKVideoNode(fileNamed: "harrypotter.mp4")
+            videoNode.play()
+            
+            let videoScene = SKScene(size: CGSize(width: 1280, height: 720))
+            videoNode.position = CGPoint(x: videoScene.size.width / 2, y: videoScene.size.height / 2)
+            videoNode.yScale = -1.0
+            videoScene.addChild(videoNode)
+            
             let plane = SCNPlane(width: imageAnchor.referenceImage.physicalSize.width, height: imageAnchor.referenceImage.physicalSize.height)
-            plane.firstMaterial?.diffuse.contents = UIColor(white: 1.0, alpha: 1.0)
+            plane.firstMaterial?.diffuse.contents = videoScene
             
             let planeNode = SCNNode(geometry: plane)
             planeNode.eulerAngles.x = -Float.pi / 2
-            node.addChildNode(planeNode)            
+            node.addChildNode(planeNode)
         }
         
         return node
